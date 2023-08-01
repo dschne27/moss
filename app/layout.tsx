9b1233a1361df1638a13d3b688b2324c84b2ecfb
Daniel <dschne@umich.edu>
2023-08-01 17:17:36 -0400

    "init"

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>Hi</nav>
+        {children}
+      </body>
     </html>
   )
 }
